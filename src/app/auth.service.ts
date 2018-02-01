a535ea7193188aae539b952d09a832c6e5c2cf1d
sushant <tsushant1996@gmail.com>
2018-02-01 22:31:01 +0530

    Prepare for heroku

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase';
 import { Observable } from 'rxjs/Observable';
+import { GoogleAuthProvider } from '@firebase/auth-types';
 
 @Injectable()
 export class AuthService {
@@ -12,7 +13,8 @@ export class AuthService {
   }
 
   login() {
-    this.afAuth.auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
+    //this.afAuth.auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
+     this.afAuth.auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
   }
 
   logout() {
